4acb58e2c5b2f76d87e6052f6e56601914766454
Rafiu Islam <md.rafiu.islam@g.bracu.ac.bd>
2021-04-05 12:55:11 +0600

    spotifywebapi added and initial login and user authentication done

---
+++ server/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const SpotifyWebApi = require("spotify-web-api-node");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post("/login", (req, res) => {
+  const code = req.body.code;
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: "http://localhost:3000",
+    clientId: "c7661c196c794073ba275881afbe59c5",
+    clientSecret: "c0a0db5aa89a4b36a5a137d0a7d0dfc7",
+  });
+
+  spotifyApi
+    .authorizationCodeGrant(code)
+    .then((data) => {
+      res.json({
+        accessToken: data.body.access_token,
+        refreshToken: data.body.refresh_token,
+        expiresIn: data.body.expires_in,
+      });
+    })
+    .catch((err) => {
+      res.sendStatus(400);
+    });
+});
+
+app.listen(3001);
