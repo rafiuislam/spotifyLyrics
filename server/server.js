5cff36ce1de6be1d8c0a2b4e170e4a7101a35ec0
Rafiu Islam <md.rafiu.islam@g.bracu.ac.bd>
2021-04-05 14:35:35 +0600

    completed login authoriation

--- server/server.js
+++ server/server.js
@@ -7,6 +7,27 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+app.post("/refresh", (req, res) => {
+  const refreshToken = req.body.refreshToken;
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: "http://localhost:3000",
+    clientId: "c7661c196c794073ba275881afbe59c5",
+    clientSecret: "c0a0db5aa89a4b36a5a137d0a7d0dfc7",
+    refreshToken,
+  });
+  spotifyApi
+    .refreshAccessToken()
+    .then((data) => {
+      res.json({
+        accessToken: data.body.access_token,
+        expiresIn: data.body.expires_in,
+      });
+    })
+    .catch(() => {
+      res.sendStatus(400);
+    });
+});
+
 app.post("/login", (req, res) => {
   const code = req.body.code;
   const spotifyApi = new SpotifyWebApi({
